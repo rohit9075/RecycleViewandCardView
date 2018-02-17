a6aad0a830f01644be38647f43e4fe6eed6e42b1
rohit9075 <rohit9075@gmail.com>
2018-02-17 15:51:48 +0530

    Initial commit

---
+++ app/src/main/java/rohit/com/recycleviewandcardview/DataAdapter.java
@@ -0,0 +1,48 @@
+package rohit.com.recycleviewandcardview;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by VIVEK on 10/15/2017.
+ */
+
+public class DataAdapter extends RecyclerView.Adapter<DataAdapter.ViewHolder> {
+    private ArrayList<String> countries;
+
+    public DataAdapter(ArrayList<String> countries) {
+        this.countries = countries;
+    }
+
+    @Override
+    public DataAdapter.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.cards_layout, viewGroup, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(DataAdapter.ViewHolder viewHolder, int i) {
+
+        viewHolder.tv_country.setText(countries.get(i));
+    }
+
+    @Override
+    public int getItemCount() {
+        return countries.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        private TextView tv_country;
+        public ViewHolder(View view) {
+            super(view);
+
+            tv_country = (TextView)view.findViewById(R.id.tv_country);
+        }
+    }
+
+}
